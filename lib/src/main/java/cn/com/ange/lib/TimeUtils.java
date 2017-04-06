674bbba98f1a30a2df626002ab95baa548a1b954
niangegelaile <13750523051@163.com>
2017-04-06 23:31:00 +0800

    初始化项目
    Signed-off-by: niangegelaile <13750523051@163.com>

---
+++ lib/src/main/java/cn/com/ange/lib/TimeUtils.java
@@ -0,0 +1,12 @@
+package cn.com.ange.lib;
+
+/**
+ * Created by liquanan on 2017/4/6.
+ * email :1369650335@qq.com
+ */
+
+public class TimeUtils {
+    public static long getTime(){
+        return System.currentTimeMillis();
+    }
+}
