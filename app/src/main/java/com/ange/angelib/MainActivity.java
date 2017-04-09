38562f98097f296eda75c6780cf5ddfd7d2c86d1
niangegelaile <13750523051@163.com>
2017-04-09 09:08:57 +0800

    成功上传到本地maven仓库
    Signed-off-by: niangegelaile <13750523051@163.com>

--- app/src/main/java/com/ange/angelib/MainActivity.java
+++ app/src/main/java/com/ange/angelib/MainActivity.java
@@ -2,6 +2,11 @@ package com.ange.angelib;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Date;
+
+import cn.com.ange.lib.TimeUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView= (TextView) findViewById(R.id.tv_time);
+        textView.setText(new Date(TimeUtils.getTime()).toLocaleString());
     }
 }
