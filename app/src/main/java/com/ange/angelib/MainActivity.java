fc6f5fa7bd59548e27f9b6c945c0a33591997911
niangegelaile <13750523051@163.com>
2017-07-29 18:41:33 +0800

    加入配置
    Signed-off-by: niangegelaile <13750523051@163.com>

--- app/src/main/java/com/ange/angelib/MainActivity.java
+++ app/src/main/java/com/ange/angelib/MainActivity.java
@@ -1,12 +1,10 @@
 package com.ange.angelib;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-import java.util.Date;
 
-import cn.com.ange.lib.TimeUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView textView= (TextView) findViewById(R.id.tv_time);
-        textView.setText(new Date(TimeUtils.getTime()).toLocaleString());
+//        textView.setText(new Date(TimeUtils.getTime()).toLocaleString());
     }
 }
